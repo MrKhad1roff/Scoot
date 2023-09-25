0621a8dea4bd0887050fc976ed3a063f490b2de1
botir <garrow088@gmail.com>
2023-09-25 19:17:21 +0500

    .

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 const titles = document.querySelectorAll('.accordion__title');
 const contents = document.querySelectorAll('.accordion__content');
 
-    titles.forEach(item => item.addEventListener('click', () => {
-        const activeContent = document.querySelector('#' + item.dataset.tab);
+titles.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab);
 
-        if (activeContent.classList.contains('active')) {
-            activeContent.classList.remove('active');
-            item.classList.remove('active');
-            activeContent.style.maxHeight = 0;
-        } else {
-            contents.forEach(element => {
-                element.classList.remove('active');
-                element.style.maxHeight = 0;
-            });
+    if (activeContent.classList.contains('active')) {
+        activeContent.classList.remove('active');
+        item.classList.remove('active');
+        activeContent.style.maxHeight = 0;
+    } else {
+        contents.forEach(element => {
+            element.classList.remove('active');
+            element.style.maxHeight = 0;
+        });
 
-            titles.forEach(element => element.classList.remove('active'));
+        titles.forEach(element => element.classList.remove('active'));
 
-            item.classList.add('active');
-            activeContent.classList.add('active');
-            activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
-        }
-    }))
+        item.classList.add('active');
+        activeContent.classList.add('active');
+        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+}))
 
- document.querySelector('[data-tab="tab-1"]').classList.add('active');
- document.querySelector('#tab-1').classList.add('active');
- document.querySelector('#tab-1').style.maxHeight = document.querySelector('#tab-1').scrollHeight + 'px';
+document.querySelector('[data-tab="tab-1"]').classList.add('active');
+document.querySelector('#tab-1').classList.add('active');
+document.querySelector('#tab-1').style.maxHeight = document.querySelector('#tab-1').scrollHeight + 'px';
 
 
 
@@ -32,30 +32,53 @@ const contents = document.querySelectorAll('.accordion__content');
 const titles1 = document.querySelectorAll('.accordion__title1');
 const contents1 = document.querySelectorAll('.accordion__content1');
 
-    titles1.forEach(item => item.addEventListener('click', () => {
-        const activeContent = document.querySelector('#' + item.dataset.tab);
+titles1.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab);
 
-        if (activeContent.classList.contains('active')) {
-            activeContent.classList.remove('active');
-            item.classList.remove('active');
-            activeContent.style.maxHeight = 0;
-        } else {
-            contents1.forEach(element => {
-                element.classList.remove('active');
-                element.style.maxHeight = 0;
-            });
+    if (activeContent.classList.contains('active')) {
+        activeContent.classList.remove('active');
+        item.classList.remove('active');
+        activeContent.style.maxHeight = 0;
+    } else {
+        contents1.forEach(element => {
+            element.classList.remove('active');
+            element.style.maxHeight = 0;
+        });
 
-            titles1.forEach(element => element.classList.remove('active'));
+        titles1.forEach(element => element.classList.remove('active'));
 
-            item.classList.add('active');
-            activeContent.classList.add('active');
-            activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+        item.classList.add('active');
+        activeContent.classList.add('active');
+        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+}))
+
+document.querySelector('[data-tab="tab-1_"]').classList.add('active');
+document.querySelector('#tab-1_').classList.add('active');
+document.querySelector('#tab-1_').style.maxHeight = document.querySelector('#tab-1_').scrollHeight + 'px';
+
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.header__list');
+const navLinks = document.querySelectorAll('.nav__list li');
+
+//Toggle nav
+
+burger.addEventListener('click', function () {
+    setTimeout(() => {
+        nav.style.opacity = "1"
+    }, 200);
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = "";
+        }else{
+            link.style.animation = `navLink 0.5s ease forwards ${index / 7 + 0.35}`
         }
-    }))
-
- document.querySelector('[data-tab="tab-1_"]').classList.add('active');
- document.querySelector('#tab-1_').classList.add('active');
- document.querySelector('#tab-1_').style.maxHeight = document.querySelector('#tab-1_').scrollHeight + 'px';
+    });
 
 
- 
+    burger.classList.toggle('toggle');
+});
+
+
